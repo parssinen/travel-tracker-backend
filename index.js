e8567fc61ba0320374785a3b8f5eb12aee0d925b
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2019-02-18 09:27:47 +0200

    another push to Heroku

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ const loginRouter = require('./controllers/login')
 const markersRouter = require('./controllers/markers')
 const config = require('./utils/config')
 
+morgan.token('data', (request, response) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(`:method :url :data :status :response-time ${'ms'}`))
+
+const url = config.mongoUrl
+mongoose.set('useFindAndModify', false)
+mongoose
+  .connect(url, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to database', config.mongoUrl)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.tokenExtractor)
@@ -19,25 +36,6 @@ app.use('/api/markers', markersRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use(express.static('build'))
-
-morgan.token('data', (request, response) => {
-  return JSON.stringify(request.body)
-})
-
-app.use(morgan(`:method :url :data :status :response-time ${'ms'}`))
-
-mongoose.set('useFindAndModify', false)
-mongoose
-  .connect(
-    'mongodb://username:password1@ds139435.mlab.com:39435/travel-tracker',
-    { useNewUrlParser: true }
-  )
-  .then(() => {
-    console.log('connected to database', config.mongoUrl)
-  })
-  .catch(error => {
-    console.log(error)
-  })
 
 app.get('/*', (request, response) => {
   response.sendFile(path.join(__dirname, './build/index.html'))
--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 let port = process.env.PORT
 let mongoUrl = process.env.MONGODB_URI
